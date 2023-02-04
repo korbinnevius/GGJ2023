d6d6eaef7a11f5fe19d9ad5dc07289ea989f90f2
LaneFrosted <LaneMacy21@gmail.com>
2023-02-04 16:14:37 -0500

    Log working

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -11,6 +11,5 @@ public class Aim : MonoBehaviour
         Vector3 PLayerLocation = (Player.position - transform.position).normalized;
       Quaternion lookRotation =  Quaternion.LookRotation(Vector3.forward,PLayerLocation);
       transform.rotation = Quaternion.Euler(0,0, lookRotation.eulerAngles.z + 90);
-      
     }
 }
--- Assets/Scripts/VineLaunch.cs
+++ Assets/Scripts/VineLaunch.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class VineLaunch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform Player;
+    public Transform Base;
+    public float Speed;
+    public float MaxRange;
+    public bool CanShoot;
+
+    private void Start()
     {
         
+        StartCoroutine(ShootRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ShootRoutine()
     {
-        
+        while (CanShoot)
+        {
+            yield return new WaitForSeconds(Random.Range(0.1f, MaxRange));
+            yield return StartCoroutine(Shoot());
+        }
+    }
+    public IEnumerator Shoot()
+    {
+        GetComponent<Aim>().enabled = false;
+        Vector3 newpos = Player.position;
+        float distance = Vector3.Distance(transform.position, newpos);
+        Vector3 start = transform.position;
+        if (distance > 0.25f)
+        {
+            float t = 0;
+            while (t < 3)
+            {
+                transform.position = Vector3.Lerp(start, newpos, t);
+                t = t + (Time.deltaTime / distance) * Speed;
+                yield return null;
+            }
+            t = 0;
+            while (t < 3)
+            {
+                transform.position = Vector3.Lerp(newpos, Base.position, t);
+                t = t + (Time.deltaTime / distance) * Speed;
+                yield return null;
+            }
+            GetComponent<Aim>().enabled = true;
+        }
     }
 }
