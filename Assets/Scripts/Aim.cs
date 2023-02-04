98c112d0907f0a01c855352a472c3cdfcbfbbc63
LaneFrosted <LaneMacy21@gmail.com>
2023-02-04 13:42:52 -0500

    Aim script works

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -7,7 +7,8 @@ public class Aim : MonoBehaviour
     public Transform Player;
     void Update()
     {
-        Vector3 PLayerLocation = Player.position - transform.position;
-        Quaternion.LookRotation(PLayerLocation,Vector3.up);
+        Vector3 PLayerLocation = (Player.position - transform.position).normalized;
+      Quaternion lookRotation =  Quaternion.LookRotation(Vector3.forward,PLayerLocation);
+      transform.rotation = Quaternion.Euler(0,0, lookRotation.eulerAngles.z + 90);
     }
 }
