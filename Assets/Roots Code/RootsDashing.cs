9614bdf983eecfddb6f4e3d38ec8bf736b5d7c33
Korbin <klnevius@gmail.com>
2023-02-04 16:27:03 -0500

    working on dash

---
+++ Assets/Roots Code/RootsDashing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootsDashing : MonoBehaviour
+{
+    public Transform orientation;
+    public Transform playerCam;
+    private Rigidbody rb;
+    
+}
